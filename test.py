5a67b71f96b54657e02d6a413ebe663da91f81b9
Wangwensong <312775704@qq.com>
2019-06-22 14:29:59 +0800

    Delete test.py
    old

--- test.py
+++
@@ -1,9 +0,0 @@
-from libtbx.easy_mp import pool_map
-from libtbx import easy_pickle
-
-
-
-pdb_pair = easy_pickle.load("X_RAY_CYRO_EM_PAIR.pkl")
-for pair in pdb_pair:
-  if pair is not None:
-    print pair
