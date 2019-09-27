fe1ecc03314da87a02d994840ab911d3aa824cdf
kingglory <312775704@qq.com>
2019-09-27 21:50:23 +0800

    to do 0

--- hydrogen_bond_restraints.py
+++ hydrogen_bond_restraints.py
@@ -17,22 +17,22 @@ from iotbx.bioinformatics.structure import summarize_blast_output
 from cctbx.array_family import flex
 
 
-def prepare_hydrogen_restraints(model):
+def prepare_hydrogen_restraints(pdb_file):
   params = homology.get_default_params()
   params.num_of_best_pdb = 1
-  pdb_code_ref = model[0:4]
-  res = homology.file_perfect_pair(model, params)
+  pdb_code_ref = pdb_file[0:4]
+  res = homology.file_perfect_pair(pdb_file, params)
   for r in res :
     data_type_A = False
     print (r.chain_ref,r.match[0].chain_id,r.match[0])
-    pdb_code_ref = model[0:4]
+    pdb_code_ref = pdb_file[0:4]
     pdb_code = r.match[0].pdb_code
     chain_id = r.match[0].chain_id
     # easy_run.call("phenix.fetch_pdb {0}".format(pdb_code))
     pdb_inp = iotbx.pdb.input(pdb_code+".pdb")
     data_type_A =pdb_inp.get_experiment_type()
     if data_type_A == False:continue
-    he = iotbx.pdb.input(file_name=model).construct_hierarchy()
+    he = iotbx.pdb.input(file_name=pdb_file).construct_hierarchy()
     hx = iotbx.pdb.input(file_name=pdb_code + ".pdb").construct_hierarchy()
     sel_e = he.atom_selection_cache().selection("protein and chain %s"%(r.chain_ref))
     he = he.select(sel_e)
@@ -40,21 +40,26 @@ def prepare_hydrogen_restraints(model):
     hx = hx.select(sel_x)
     chain_A = (he.only_chain())
     chain_B = (hx.only_chain())
-    align_tow_chain(chain_A,chain_B)
-def align_tow_chain(chain_A,chain_B):
+    #phil_obj = align_tow_chain(chain_A,chain_B,phil_obj)
+def align_tow_chain(chain_A,chain_B,phil_obj):
   se = chain_A.as_padded_sequence()
   sx = chain_B.as_padded_sequence()
   align = mmtbx.alignment.align(
     seq_a = se,
     seq_b = sx)
   alignment = align.extract_alignment()
-  matches = alignment.matches()
+  matches = alignment.identity_matches()
+  a_seq = alignment.a
+  b_seq = alignment.b
+  ai_seq = alignment.i_seqs_a
+  bi_seq = alignment.i_seqs_b
+  print (dir(alignment))
+  print (a_seq)
   print (matches)
-  equal = matches.count("|")
-  similar = matches.count("*")
-  print ( "Number of matches after alignment: ", equal )
-
-
+  print (b_seq)
+  print (ai_seq)
+  print (bi_seq)
+  return phil_obj
 if __name__ == '__main__':
     start = time.time()
     prepare_hydrogen_restraints('6d9h.pdb')
