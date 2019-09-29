49f778762728890c3c156c7ef95b9314c7e9d9c4
kingglory <312775704@qq.com>
2019-09-29 16:40:11 +0800

    nice!

--- hydrogen_bond_restraints.py
+++ hydrogen_bond_restraints.py
@@ -1,21 +1,29 @@
+from __future__ import absolute_import, division, print_function
+import iotbx.pdb
+import mmtbx.model
+from libtbx.utils import null_out
+from libtbx.test_utils import approx_equal
 from phenix.programs import homology
 from mmtbx.nci import hbond
 import time
 import mmtbx
 import mmtbx.alignment
 from libtbx import easy_run
-from libtbx import group_args
 import iotbx.pdb
 import iotbx.pdb.fetch
 import iotbx.phil
-from libtbx import easy_pickle
-from libtbx.utils import Sorry
-from iotbx import bioinformatics
 import iotbx.bioinformatics.pdb_info
-from iotbx.bioinformatics import local_blast
-from iotbx.bioinformatics.structure import summarize_blast_output
-from cctbx.array_family import flex
+from mmtbx.utils import run_reduce_with_timeout
 
+def add_hydrogen_for_hierarchy(hierarchy_old):
+  rr = run_reduce_with_timeout(
+    stdin_lines=hierarchy_old.as_pdb_string().splitlines(),
+    file_name=None,  # "model.pdb.gz",
+    parameters="-oh -his -flip -keep -allalt -pen9999 -",
+    override_auto_timeout_with=None)
+  pdb_inp = iotbx.pdb.input(source_info=None, lines=rr.stdout_lines)
+  hierarchy_new = pdb_inp.construct_hierarchy()
+  return hierarchy_new
 
 def prepare_hydrogen_restraints(pdb_file):
   params = homology.get_default_params()
@@ -24,7 +32,7 @@ def prepare_hydrogen_restraints(pdb_file):
   res = homology.file_perfect_pair(pdb_file, params)
   for r in res :
     data_type_A = False
-    print (r.chain_ref,r.match[0].chain_id,r.match[0])
+    #print (r.chain_ref,r.match[0].chain_id,r.match[0])
     pdb_code_ref = pdb_file[0:4]
     pdb_code = r.match[0].pdb_code
     chain_id = r.match[0].chain_id
@@ -40,8 +48,12 @@ def prepare_hydrogen_restraints(pdb_file):
     hx = hx.select(sel_x)
     chain_A = (he.only_chain())
     chain_B = (hx.only_chain())
+    hx_h = add_hydrogen_for_hierarchy(hx)
+    align_tow_chain(chain_A, chain_B,hx_h.as_pdb_string())
     #phil_obj = align_tow_chain(chain_A,chain_B,phil_obj)
-def align_tow_chain(chain_A,chain_B,phil_obj):
+def align_tow_chain(chain_A,chain_B,str_chain, file_name="hbond.eff",
+              distance_ideal=None, sigma_dist=0.1,angle_ideal = None,
+              sigma_angle=2, use_actual=True):
   se = chain_A.as_padded_sequence()
   sx = chain_B.as_padded_sequence()
   align = mmtbx.alignment.align(
@@ -53,16 +65,81 @@ def align_tow_chain(chain_A,chain_B,phil_obj):
   b_seq = alignment.b
   ai_seq = alignment.i_seqs_a
   bi_seq = alignment.i_seqs_b
+  '''
   print (dir(alignment))
+  print(dir(matches))
   print (a_seq)
   print (matches)
   print (b_seq)
   print (ai_seq)
   print (bi_seq)
-  return phil_obj
+  '''
+  match_id=[]
+  i=1
+  for m in matches:
+    if m == "|":
+        match_id.append(i)
+    i = i + 1
+  #print (match_id)
+  pdb_inp = iotbx.pdb.input(source_info=None, lines=str_chain)
+  model = mmtbx.model.manager(
+    model_input=pdb_inp,
+    process_input=True,
+    log=null_out())
+  result = mmtbx.nci.hbond.find(model=model).get_result()
+  f = "chain %s and resseq %s and name %s"
+  top = "a"
+  for r in result:
+    h = f % (r.atom_H.chain, r.atom_H.resseq, r.atom_H.name)
+    a = f % (r.atom_A.chain, r.atom_A.resseq, r.atom_A.name)
+    d = f % (r.atom_D.chain, r.atom_D.resseq, r.atom_D.name)
+    if (not use_actual):
+      if (r.d_HA < 2.5):
+        dt = 2.05
+      else:
+        dt = 2.8
+      if (r.a_DHA < 130):
+        at = 115
+      else:
+        at = 160
+    else:
+      dt = r.d_HA
+      at = r.a_DHA
+    dis = """bond {
+            atom_selection_1 = %s
+            atom_selection_2 = %s
+            symmetry_operation = %s
+            distance_ideal = %f
+            sigma = 0.05
+            }
+      """ % (h, a, str(r.symop), dt)
+    if (str(r.symop) != "x,y,z"): continue
+    ang = """angle {
+            atom_selection_1 = %s
+            atom_selection_2 = %s
+            atom_selection_3 = %s
+            angle_ideal = %f
+            sigma = 5
+            }
+      """ % (a, h, d, at)
+    print(dis)
+    print(ang)
+    base = dis + ang
+    top = top + base
+  top_final = top[1:]
+  print (top_final)
+  '''
+  with open("hbond.eff", 'w') as fileobject:
+    f = fileobject.read()
+    f.replace(chain_A.id,chain_B.id)
+
+  '''
+
+
+   #return phil_obj
 if __name__ == '__main__':
     start = time.time()
     prepare_hydrogen_restraints('6d9h.pdb')
     end = time.time()
     time_cost = (end - start)
-    print "it cost % seconds" % time_cost
+    print ("it cost % seconds" % time_cost)
