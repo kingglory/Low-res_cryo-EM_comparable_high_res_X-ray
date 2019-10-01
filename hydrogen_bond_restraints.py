4711732a571fb6ca72ae72eadfbae114d5033fe8
kingglory <312775704@qq.com>
2019-10-01 20:10:42 +0800

    problem:can't replace the matchment chain informations rightly(alignment)

--- hydrogen_bond_restraints.py
+++ hydrogen_bond_restraints.py
@@ -33,65 +33,70 @@ def prepare_hydrogen_restraints(pdb_file):
   phil_result= {}
   for r in res :
     data_type_A = False
-    #print (r.chain_ref,r.match[0].chain_id,r.match[0])
+    print (r.chain_ref,r.match[0].chain_id,r.match[0])
     pdb_code_ref = pdb_file[0:4]
-    pdb_code = r.match[0].pdb_code
+    pdb_code = r.match[0].pdb_code.lower()
     chain_id = r.match[0].chain_id
     match_info = pdb_code_ref+"_"+r.chain_ref+"_"+pdb_code+"_"+chain_id
     print (match_info)
-    # easy_run.call("phenix.fetch_pdb {0}".format(pdb_code))
-    pdb_inp = iotbx.pdb.input(pdb_code+".pdb")
-    data_type_A =pdb_inp.get_experiment_type()
-    if data_type_A == False:continue
+    #easy_run.call("phenix.fetch_pdb {0}".format(pdb_code))
+    # there are two conformers in chian B 233 5kdo.pdb,
+    # so cann't keep only one conformer situation in hierarchy
+    easy_run.call("phenix.pdbtools {0} remove_alt_confs=True".format(pdb_code+".pdb"))
+    pdb_inp = iotbx.pdb.input(pdb_code+".pdb_modified.pdb")
+    data_type_X =pdb_inp.get_experiment_type()
+    if data_type_X == False:continue
     he = iotbx.pdb.input(file_name=pdb_file).construct_hierarchy()
-    hx = iotbx.pdb.input(file_name=pdb_code + ".pdb").construct_hierarchy()
+    hx = iotbx.pdb.input(file_name=pdb_code + ".pdb_modified.pdb").construct_hierarchy()
     sel_e = he.atom_selection_cache().selection("protein and chain %s"%(r.chain_ref))
     he = he.select(sel_e)
     sel_x = hx.atom_selection_cache().selection("protein and chain %s" % (chain_id))
     hx = hx.select(sel_x)
-    chain_A = (he.only_chain())
-    chain_B = (hx.only_chain())
+    chain_E = (he.only_chain())
+    chain_X = (hx.only_chain())
     hx_h = add_hydrogen_for_hierarchy(hx)
-    #align_tow_chain(chain_A, chain_B,hx_h.as_pdb_string())
-    phil_obj = align_tow_chain(chain_A,chain_B,hx_h.as_pdb_string())
-    '''TO DO???????:
-       replace chian A atoms informations by B atoms informations (res_id,chian_id)
-    '''
+    phil_obj = align_tow_chain(chain_E,chain_X,hx_h.as_pdb_string())
     phil_result[match_info] = phil_obj
   return phil_result
-      
 
-def align_tow_chain(chain_A,chain_B,str_chain,
+def dump_phil_file(phil_result,for_phenix_refine=True):
+  top = """refinement{
+    geometry_restraints.edits{
+      %s
+    }
+  }
+      """
+  for match_info, phil_obj in phil_result.items():
+    top_str = top % phil_obj
+    file_name = match_info + ".eff"
+    with open(file_name, 'w') as fileobject:
+      fileobject.write(top_str)
+
+def align_tow_chain(chain_E,chain_X,str_chain_X,
               distance_ideal=None, sigma_dist=0.1,angle_ideal = None,
               sigma_angle=2, use_actual=True):
-  se = chain_A.as_padded_sequence()
-  sx = chain_B.as_padded_sequence()
+  se = chain_E.as_padded_sequence()
+  sx = chain_X.as_padded_sequence()
   align = mmtbx.alignment.align(
     seq_a = se,
     seq_b = sx)
   alignment = align.extract_alignment()
-  #print (dir(alignment))
-  matches = alignment.identity_matches()
-  match_part = alignment.exact_match_selections()
-  a_seq = alignment.a
-  b_seq = alignment.b
-  ai_seq = alignment.i_seqs_a
-  bi_seq = alignment.i_seqs_b
-  '''
-  print (a_seq)
-  print (matches)
-  print (b_seq)
-  print (ai_seq)
-  print (bi_seq)
-  '''
-  match_id=[]
-  i=1
-  for m in matches:
-    if m == "|":
-        match_id.append(i)
-    i = i + 1
-  #print (match_id)
-  pdb_inp = iotbx.pdb.input(source_info=None, lines=str_chain)
+  a_seq = alignment.i_seqs_a
+  b_seq = alignment.i_seqs_b
+  identity_matches = alignment.identity_matches()
+  match = alignment.matches()
+  alignment.pretty_print()
+  #print (identity_matches)
+  #print (a_seq)
+  #print (match)
+  #print (b_seq)
+  (i_seqs,j_seqs) = alignment.exact_match_selections()
+  match_result = {}
+  reses_E = chain_E.residues()
+  reses_X = chain_X.residues()
+  for i in range(len(i_seqs)):
+    match_result[i_seqs[i]]=j_seqs[i]
+  pdb_inp = iotbx.pdb.input(source_info=None, lines=str_chain_X)
   model = mmtbx.model.manager(
     model_input=pdb_inp,
     process_input=True,
@@ -100,9 +105,15 @@ def align_tow_chain(chain_A,chain_B,str_chain,
   f = "chain %s and resseq %s and name %s"
   top = "a"
   for r in result:
-    h = f % (r.atom_H.chain, r.atom_H.resseq, r.atom_H.name)
-    a = f % (r.atom_A.chain, r.atom_A.resseq, r.atom_A.name)
-    d = f % (r.atom_D.chain, r.atom_D.resseq, r.atom_D.name)
+    for (i) in range(min(len(i_seqs),len(j_seqs))):
+      if reses_X[i] is None:continue
+      if reses_E[match_result[i]] is None:continue
+      if r.atom_H.resseq == reses_X[i].resseq:
+        h = f % (reses_E[match_result[i]].parent().parent().id, reses_E[match_result[i]].resseq, r.atom_H.name)
+      if r.atom_A.resseq == reses_X[i].resseq:
+        a = f % (reses_E[match_result[i]].parent().parent().id, reses_E[match_result[i]].resseq, r.atom_A.name)
+      if r.atom_D.resseq == reses_X[i].resseq:
+        d = f % (reses_E[match_result[i]].parent().parent().id, reses_E[match_result[i]].resseq, r.atom_D.name)
     if (not use_actual):
       if (r.d_HA < 2.5):
         dt = 2.05
@@ -135,29 +146,14 @@ def align_tow_chain(chain_A,chain_B,str_chain,
     base = dis + ang
     top = top + base
   top_final = top[1:]
-  top_final.replace(chain_A.id,chain_B.id)
-  #print (top_final)
-  return top_final
-def dump_phil_file(pdb_file,for_phenix_refine=True):
-  top = """refinement{
-    geometry_restraints.edits{
-      %s
-    }
-  }
-      """
-  phil_result = prepare_hydrogen_restraints(pdb_file)
-  for match_info, phil_obj in phil_result.items():
-    top_str = top % phil_obj
-    #print(top_str)
-    file_name = match_info + ".eff"
-    with open(file_name, 'w') as fileobject:
-      fileobject.write(top_str)
+  dump_phil_file(top_final)
+
 
     #return phil_obj
 if __name__ == '__main__':
     start = time.time()
-    #prepare_hydrogen_restraints('6d9h.pdb')
-    dump_phil_file('6d9h.pdb')
+    prepare_hydrogen_restraints('6d9h.pdb')
+    #dump_phil_file('6d9h.pdb')
     end = time.time()
     time_cost = (end - start)
     print ("it cost % seconds" % time_cost)
