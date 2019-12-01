3bc9b97b8c146088ea848338c3c07cdd53771ea8
kingglory <312775704@qq.com>
2019-12-01 20:50:52 +0800

    process is coming

--- show_first_10_percent_draw_picture.py
+++ show_first_10_percent_draw_picture.py
@@ -48,14 +48,47 @@ def exercise_1():
   plt.pie(X,labels=labels,autopct="%1.2f%%")
   plt.title("all_high_percent")
   plt.show()
+def exercise_2():
+  f3= easy_pickle.load("ss-percent_3_6.pkl")
+  print (len(f3))
 
-f3= easy_pickle.load("ss-percent_3_6.pkl")
-print (len(f3))
 
+def exercise_3():
+  f4= easy_pickle.load("ss-percent_all.pkl")
+  f_4 = sorted(f4.items(), key=itemgetter(1), reverse=True)
+  labels = ["matching", "unmatching"]
+  X = [len(f_4), whole_num_of_cryo_EM - len(f_4)]
+  fig = plt.figure()
+  plt.pie(X, labels=labels, autopct="%1.2f%%")
+  plt.title("all_pie")
+  plt.show()
+  list = []
+  for key, value in f4.items():
+    list.append(value)
+  plt.hist(list, bins=12, color="c", density=False, range=(0, 100))
+  plt.title("all_hist")
+  plt.show()
+  list_0_20 = []
+  list_20_40 = []
+  list_40_60 = []
+  list_60_100 = []
+  for i in list:
+    if 0 <= i < 20:
+      list_0_20.append(i)
+    if 20 <= i < 40:
+      list_20_40.append(i)
+    if 40 <= i < 60:
+      list_40_60.append(i)
+    if 60 <= i <= 100:
+      list_60_100.append(i)
+  labels = ["0~20", "20~40", "40~60", "60~100"]
+  Y = [len(list_0_20), len(list_20_40), len(list_40_60),
+       len(list_60_100)]
+  print (Y)
+  fig = plt.figure()
+  plt.pie(Y, labels=labels, autopct="%1.2f%%")
+  plt.title("all_matching_percent")
+  plt.show()
 
-
-f4= easy_pickle.load("ss-percent.pkl")
-print (len(f4))
-
-exercise_0()
+exercise_3()
 #exercise_1()
