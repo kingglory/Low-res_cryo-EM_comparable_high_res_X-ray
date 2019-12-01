a27c67f37b3cccdd59b447f41303822a95e7f7d6
kingglory <312775704@qq.com>
2019-12-01 20:06:20 +0800

    process is coming

--- show_first_10_percent_draw_picture.py
+++ show_first_10_percent_draw_picture.py
@@ -12,8 +12,33 @@ def exercise_0():
   plt.pie(X,labels=labels,autopct="%1.2f%%")
   plt.title("3_6_high_percent")
   plt.show()
-  plt.savefig("PieChart_3_6_high.jpg")
-  print (f_1)
+  list=[]
+  for key,value in f1.items():
+    list.append(value)
+  plt.hist(list,bins=12,color="c",density=False,range=(0,100))
+  plt.title("3_6_high_hist")
+  plt.show()
+  list_0_20=[]
+  list_20_40=[]
+  list_40_60=[]
+  list_60_100=[]
+  for i in list:
+    if 0<=i<20:
+      list_0_20.append(i)
+    if 20<=i<40:
+      list_20_40.append(i)
+    if 40<=i<60:
+      list_40_60.append(i)
+    if 60<=i<=100:
+      list_60_100.append(i)
+  labels = ["0~20","20~40","40~60","60~100"]
+  Y = [len(list_0_20),len(list_20_40),len(list_40_60),
+      len(list_60_100)]
+  print (Y)
+  fig = plt.figure()
+  plt.pie(Y, labels=labels, autopct="%1.2f%%")
+  plt.title("3_6_matching_percent")
+  plt.show()
 def exercise_1():
   f2= easy_pickle.load("ss-percent_all_high_res.pkl")
   f_2 = sorted(f2.items(),key=itemgetter(1),reverse=True)
@@ -23,8 +48,7 @@ def exercise_1():
   plt.pie(X,labels=labels,autopct="%1.2f%%")
   plt.title("all_high_percent")
   plt.show()
-  plt.savefig("PieChart_all_high.jpg")
-  print (f_2)
+
 f3= easy_pickle.load("ss-percent_3_6.pkl")
 print (len(f3))
 
