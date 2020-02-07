9ae78338ce2c5e20a3fbd2e46a6b57d66b146651
Wangwensong <312775704@qq.com>
2020-02-07 19:49:01 +0800

    update test

--- test_script/test_alignment.py
+++ test_script/test_alignment.py
@@ -11,14 +11,24 @@ from libtbx import easy_run
 from phenix.programs import homology
 import iotbx.bioinformatics.pdb_info
 
+def align_tow_chain(hierarchy_A,hierarchy_B):
+  for chain_A in hierarchy_A.chains():
+    for chain_B in hierarchy_B.chains():
+      se = chain_A.as_padded_sequence()
+      sx = chain_B.as_padded_sequence()
+      #print ("\n",se,"\n",sx)
+      align = mmtbx.alignment.align(seq_a = se,
+                                    seq_b = sx)
+      alignment = align.extract_alignment()
+      #print (dir(alignment))
+      (i_seqs,j_seqs) = alignment.exact_match_selections()
+      match_X_E = {}
+      for (i,j) in  zip(i_seqs,j_seqs):
+        print (i,j)
+        match_X_E[j+1]=i+1
+      print (match_X_E)
 
-def test_0():
-  '''
-  easy_run.call("phenix.pdbtools {0} remove_alt_confs=True".format("A.pdb"))
-  easy_run.call("phenix.pdbtools {0} remove_alt_confs=True".format("B.pdb"))
-  pdb_inp_B = iotbx.pdb.input("B.pdb_modified.pdb")
-  pdb_inp_A = iotbx.pdb.input("A.pdb_modified.pdb")
-  '''
+if __name__ == '__main__':
   pdb_inp_B = iotbx.pdb.input("B.pdb")
   pdb_inp_A = iotbx.pdb.input("A.pdb")
   h_A = homology.get_hierarchy(pdb_inp_A)
@@ -27,16 +37,4 @@ def test_0():
     model_input=pdb_inp_B,
     process_input=True,
     log=null_out())
-  as_pymol(model, prefix="B")
-  prepare_hydrogen_restraints(hierarchy=h_A,hierarchy_pair=h_B,pdb_id="B")
-
-
-if __name__ == '__main__':
-  start = time.time()
-  test_0()
-  end = time.time()
-  time_cost = (end - start)
-  print("it cost % seconds" % time_cost)
-
-
-
+  align_tow_chain(h_A,h_B)
